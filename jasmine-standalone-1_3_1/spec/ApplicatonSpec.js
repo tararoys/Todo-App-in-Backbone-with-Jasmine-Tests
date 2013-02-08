dde7b6e955633195a2d696f44e62b448a66da8d2
Mobile User <mobile@Taras-iPad.(none)>
2013-02-07 19:41:06 -0600

    The laws of test driven design state that you write a test before you write any code.  As this commit shows, you do not need any source code in your source folder to run Jasmine's Specrunner.  All you need is a single test.  The first test I chose to write was a test to see if the testing framework was working, becuse without a working framework, no design can happen. i Things learned:
    The describe function describes what is being tested in a suite of tests.  You might say the describe functionbis the suite. vyou need o have at least one suite to run SpecRunner, and that suite has o contain obe unit test. The it function describes one unit test, and inside the unit test, he expect function describes what you expect it to do. The whole thing is surprisingly human-readable.  I also learned how to use the toBeTruthy matcher.

---
+++ jasmine-standalone-1_3_1/spec/ApplicatonSpec.js
@@ -0,0 +1,7 @@
+describe ("An Application that stores todo lists", function(){
+	it("should have a working testing framework", function(){
+		//if this test shows up when you run specrunner, you know jasmine is working
+		var jasmine_works=true;
+		expect(jasmine_works).toBeTruthy();
+	 });
+});
