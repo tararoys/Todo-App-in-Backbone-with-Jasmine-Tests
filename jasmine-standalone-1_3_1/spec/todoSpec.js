d293fa76d383fc0e1ee09dae0272e41f50f1a276
Mobile User <mobile@Taras-iPad.(none)>
2013-02-08 15:35:13 -0600

    Learned how to add an attribute when creating a model.  It seems strange that you on't have to define the attributes for a model when you make the constructorLearned how to add an attribute when creating a model.  It seems strange that you on't have to define the attributes for a model when you make the constructor..

--- jasmine-standalone-1_3_1/spec/todoSpec.js
+++ jasmine-standalone-1_3_1/spec/todoSpec.js
@@ -1,10 +1,19 @@
-describe ("A Todo model to create individual todo items", function(){
+describe ("A Todo model (to create individual todo list items)", function(){
 	
 	
 	it("should be able to create an instance of a todo item", function(){ 
 		
 		var todoitem = new Todo();
 		expect(todoitem).toBeDefined();
+	});
+	it("should have a caption", function(){ 
+		var todoitem = new Todo({
+			caption: "Todo item"
+		}
+		);
+		expect(todoitem.get("caption")).toBeDefined();
+		
+	
 	});
 	
 });
