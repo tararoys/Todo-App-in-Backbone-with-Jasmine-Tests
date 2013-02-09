7215ebab4d2c88a2f9dcc46694e3f925bd367aa3
Mobile User <mobile@Taras-iPad.(none)>
2013-02-09 11:36:15 -0600

    Added a test to see if the todolist collection was storing todo models.  learned that collections are javascript objects, and the collection sores the type of model in the collection as a javadcript property, so you cand find out what type of model is in the collection by typing todo_list.model  just like acessing a property in a normal javadcript object.

--- jasmine-standalone-1_3_1/spec/todosSpec.js
+++ jasmine-standalone-1_3_1/spec/todosSpec.js
@@ -1,8 +1,13 @@
 describe ("A todo list (Aka a collection of todo items)", function(){
 	it("should be able to create a new collection", function(){
 		
-		
 		expect(Backbone.Collection.extend({})).toBeDefined();
+		expect(TodoList).toBeDefined();
 	
 	 });
+	it("should contain todo models ", function(){ 
+		var todo_list1 = new TodoList();
+		expect(todo_list1.model).toBe(Todo);
+	
+	});
 });
