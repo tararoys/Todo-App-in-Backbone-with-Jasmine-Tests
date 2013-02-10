4572c81f3f85f9f3aa6d03ec08c965663e53d802
Mobile User <mobile@Taras-iPad.(none)>
2013-02-10 13:04:22 -0600

    Added tests. -add todo items to todo list -remove items from todo list
    I may be repeating myself in the setup for these tests.
    
    Should I be testing for things backbone does as a feature? I didn't have to write any production code to check if this worked.  answer: yes I should write tests like these, especially since I'm using them to learn how backbone works in the fist place.

--- jasmine-standalone-1_3_1/spec/todosSpec.js
+++ jasmine-standalone-1_3_1/spec/todosSpec.js
@@ -15,4 +15,30 @@ describe ("A todo list (Aka a collection of todo items)", function(){
 		expect(todo_list1.localStorage).toBeDefined();
 	
 	 });
+	 it("should add a model to the collection ", function(){ 
+	 	var todo_list1 = new TodoList();
+	 	var my_todo = new Todo({
+	 	 caption: 'Add a todo to the todolist', 
+	 	 completed: false,
+	 	 id:1
+	 	 });
+	 	todo_list1.add(my_todo);
+	 	expect(todo_list1.get(1)).toBe(my_todo);
+	 
+	 });
+	 it("should delete a model from the collection ", function(){
+	 	var todo_list1 = new TodoList();
+	 	var my_todo = new Todo({
+	 	 caption: 'delete a todo to the todolist', 
+	 	 completed: false,
+	 	 id:1
+	 	 });
+	 	todo_list1.add(my_todo);
+	 	todo_list1.remove(1);
+	 	
+	 	expect(todo_list1.get(1)).not.toBeDefined();
+	 
+	  });
+	  
+	 
 });
